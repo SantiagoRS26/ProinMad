18135bc2ad09ec131b98a2e9b9ebb6e643a9332e
SantiagoRS26 <santiagors2611@gmail.com>
2022-07-29 23:46:53 -0500

    Cambios parciales

--- js/main.js
+++ js/main.js
@@ -41,40 +41,8 @@ function main() {
       $(".navbar-collapse").collapse('hide');
     }
   });
-	
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
 
-    });
-	
-    // Nivo Lightbox 
-    $('.portfolio-item a').nivoLightbox({
-            effect: 'slideDown',  
-            keyboardNav: true,                            
-        });
+
 
 }());
 
