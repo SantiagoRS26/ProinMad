ce6aa38d60cc17760030f81fa0c01134edc8d7b7
SantiagoRS26 <santiagors2611@gmail.com>
2022-07-31 21:41:28 -0500

    Formulario contacto corregido

--- js/contact_me.js
+++ js/contact_me.js
@@ -3,21 +3,21 @@ $(function() {
     $("input,textarea").jqBootstrapValidation({
         preventSubmit: true,
         submitError: function($form, event, errors) {
-            // additional error messages or events
+            // mensajes de error o eventos adicionales
         },
         submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
-            // get values from FORM
+            event.preventDefault(); // evitar el comportamiento de envío predeterminado
+            // obtener valores del formulario
             var name = $("input#name").val();
             var email = $("input#email").val();
             var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
+            var firstName = name; // Para mensaje de éxito/fracaso
+            // Compruebe si hay espacios en blanco en el nombre para el mensaje de éxito/fallo
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "../mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
@@ -31,7 +31,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong> Tu mensaje ha sido enviado. </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -43,7 +43,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Lo siento " + firstName + ", parece que el servidor de correo no responde. ¡Por favor, inténtelo de nuevo más tarde!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,24 +1,33 @@
 <?php
-// Check for empty fields
+
+// Verificar campos vacios
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "¡No se proporcionaron argumentos!";
-	/* return false; */
+	return false;
    }
 	
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
-// Create the email and send the message
-$to = 'info@proinmad.com.co'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+// Crear el correo electrónico y enviar el mensaje
+$to = 'santiagors2611@gmail.com'; 
+$email_subject = "Formulario de contacto del sitio web:  $name";
 $email_body = "Ha recibido un nuevo mensaje del formulario de contacto de su sitio web.\n\n"."Aquí están los detalles:\n\Nombre: $name\n\Correo: $email_address\n\Mensaje:\n$message";
-$headers = "From: nocontestar@proinmad.com.co\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: nocontestar@proinmad.com.co\n";
 $headers .= "Reply-To: $email_address";	
-mail($to,$email_subject,$email_body,$headers);
-/* return true;	 */		
+
+$prueba = mail($to,$email_subject,$email_body,$headers);
+if($prueba){
+   echo '<h1>Mensaje enviado correctamente</h1>';
+}
+else
+{
+   echo '<h1>No se envio</h1>';
+}
+return true;		
 ?>
