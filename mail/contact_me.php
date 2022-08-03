c4d7b1862a1d56e4177b7ca4b3ffda610c05b836
SantiagoRS26 <santiagors2611@gmail.com>
2022-08-02 22:17:02 -0500

    Integracion boton flotante whatsapp

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -15,19 +15,12 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Crear el correo electrónico y enviar el mensaje
-$to = 'santiagors2611@gmail.com'; 
+$to = 'gerencia@proinmad.com.co'; 
 $email_subject = "Formulario de contacto del sitio web:  $name";
 $email_body = "Ha recibido un nuevo mensaje del formulario de contacto de su sitio web.\n\n"."Aquí están los detalles:\n\Nombre: $name\n\Correo: $email_address\n\Mensaje:\n$message";
 $headers = "From: nocontestar@proinmad.com.co\n";
 $headers .= "Reply-To: $email_address";	
 
-$prueba = mail($to,$email_subject,$email_body,$headers);
-if($prueba){
-   echo '<h1>Mensaje enviado correctamente</h1>';
-}
-else
-{
-   echo '<h1>No se envio</h1>';
-}
+mail($to,$email_subject,$email_body,$headers);
 return true;		
 ?>
